b4e93ffb9d6cab6500ae72557914d590d4703c4f
alex11kr <56466355+alex11kr@users.noreply.github.com>
2020-10-15 10:55:36 +0600

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,27 +1,43 @@
-// Create a trust manager that does not validate certificate chains
-TrustManager[] trustAllCerts = new TrustManager[] { 
-    new X509TrustManager() {     
-        public java.security.cert.X509Certificate[] getAcceptedIssuers() { 
-            return new X509Certificate[0];
-        } 
-        public void checkClientTrusted( 
-            java.security.cert.X509Certificate[] certs, String authType) {
-            } 
-        public void checkServerTrusted( 
-            java.security.cert.X509Certificate[] certs, String authType) {
-        }
-    } 
-}; 
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.GeneralSecurityException;
+import java.security.cert.X509Certificate;
+
+public class Program {
+    public static void main(String[] args) throws IOException {
+
+        // Create a trust manager that does not validate certificate chains
+        TrustManager[] trustAllCerts = new TrustManager[] {
+                new X509TrustManager() {
+                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                        return new X509Certificate[0];
+                    }
+                    public void checkClientTrusted(
+                            java.security.cert.X509Certificate[] certs, String authType) {
+                    }
+                    public void checkServerTrusted(
+                            java.security.cert.X509Certificate[] certs, String authType) {
+                    }
+                }
+        };
 
 // Install the all-trusting trust manager
-try {
-    SSLContext sc = SSLContext.getInstance("SSL"); 
-    sc.init(null, trustAllCerts, new java.security.SecureRandom()); 
-    HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-} catch (GeneralSecurityException e) {
-} 
+        try {
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (GeneralSecurityException e) {
+        }
 // Now you can access an https URL without having the certificate in the truststore
-try { 
-    URL url = new URL("https://hostname/index.html"); 
-} catch (MalformedURLException e) {
-} 
+        try {
+            URL url = new URL("https://localhost");
+        } catch (MalformedURLException e) {
+        }
+    }
+}
+
